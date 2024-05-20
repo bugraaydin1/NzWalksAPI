bc02c47b17f3f849261b2b06f26110a7ea70985e
Ahmet Buğra Aydın <bugraaydin1@hotmail.com>
2024-05-20 22:46:59 +0300

    Init project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.EnableTryItOutByDefault();
+    });
+}
+
+app.MapControllers();
+app.UseHttpsRedirection();
+
+app.Run();
+
+
